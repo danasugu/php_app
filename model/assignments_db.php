88d1181e11fa6be2a655509a87549443180a3bb7
Dana Sugu <dana.sugu@gmail.com>
2021-07-09 08:40:02 +0300

    Update assignments_db.php

--- model/assignments_db.php
+++ model/assignments_db.php
@@ -10,8 +10,18 @@
     }
     $statement = $db->prepare($query);
     $statement->bindValue(':course_id', $course_id);
+    $statement->execute();
     $assignments = $statement->fetchAll();
     $statement->closeCursor();
     return $assignments;
+  }
+
+  function delete_assignment($assignment_id) {
+    global $db;
+    $query = 'DELETE FROM assignments WHERE ID = :assign_id';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':assign_id', $assignment_id);
+    $statement->execute();
+    $statement->closeCursor();
 
   }
