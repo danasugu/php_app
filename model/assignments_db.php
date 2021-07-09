717cf8468976d0042e080499a040f1e8e2e8513b
Dana Sugu <dana.sugu@gmail.com>
2021-07-09 08:24:05 +0300

    Create assignments_db.php

---
+++ model/assignments_db.php
@@ -0,0 +1,11 @@
+// functions to interract with the model for the assignments table 
+<?php
+
+  function get_assignments_by_course($course_id) {
+    global $db;
+    if($course_id) {
+        $query = 'SELECT A.ID, A.DESCRIPTION, C.courseName FROM assignments A LEFT JOIN courses C ON A.courseID = C.courseID WHERE A.courseID = :course_id ORDER BY A.ID';
+    }else{
+
+    }
+  }
